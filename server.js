6043fe6384204167239bae6ba3dedd5cc3a8cc1a
Carl Janz <carl.janz@gmail.com>
2019-05-02 12:01:12 -0700

    stuff

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/freebiesdb";
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb://heroku_dh8zm9nj:q1a8nrj5qm6iri3i8eal1bvlg5@ds151086.mlab.com:51086/heroku_dh8zm9nj";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/freebiesdb";
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
