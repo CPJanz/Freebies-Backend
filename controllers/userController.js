17e4f30fd88e783ee779924296833483c7edbfc7
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-02 11:26:05 -0700

    added a root route

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,24 @@ const db = require("../models");
 
 module.exports = {
 
-    newUser: function(req, res) {
+    allUsers: (req, res) => {
+        db.User.find()
+            .then(dbusers => res.json(dbusers))
+            .catch(err => res.status(422).json(err));
+    },
+    newUser: (req, res) => {
         db.User.create({
             email: req.body.email
             })
             .then(dbuser => res.json(dbuser))
             .catch(err => res.status(422).json(err));
     },
-    signIn: function(req, res) {
+    signIn: (req, res) => {
         db.User.findById({ id: req.params.id })
             .then(dbuser => res.json(dbuser))
             .catch(err => res.status(422).json(err));
     },
-    postedList: function(req, res) {
+    postedList: (req, res) => {
         db.User.findById({ id: req.params.id })
             .populate("item")
             .then(dbuser => res.json(dbuser))
