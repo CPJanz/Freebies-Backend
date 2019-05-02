bdd89651a213aa08fbcad7487cf38e7febd88310
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-02 12:43:46 -0700

    added _ to id and / to signIn route

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,12 +15,12 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     signIn: (req, res) => {
-        db.User.findById({ id: req.params.id })
+        db.User.findById({ _id: req.params.id })
             .then(dbuser => res.json(dbuser))
             .catch(err => res.status(422).json(err));
     },
     postedList: (req, res) => {
-        db.User.findById({ id: req.params.id })
+        db.User.findById({ _id: req.params.id })
             .populate("item")
             .then(dbuser => res.json(dbuser))
             .catch(err => res.status(422).json(err));
