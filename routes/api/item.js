691f683f560117de9f8dcf06d040d129ae4a3bdc
Carl Janz <carl.janz@gmail.com>
2019-05-03 14:51:44 -0700

    Updated item get request to return in the properly sorted order. Added giver item route

--- routes/api/item.js
+++ routes/api/item.js
@@ -6,7 +6,8 @@ router.route("/").get(itemController.findNearby);
 router
   .route("/:id")
   .post(itemController.create)
-  .put(itemController.taken);
+  .put(itemController.taken)
+  .get(itemController.findGiven);
 
 router.route("/repost/:id").put(itemController.repost);
 
