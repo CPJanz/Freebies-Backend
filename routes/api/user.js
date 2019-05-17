8c4b1a6dad6334e54390eacd7da77eea20cb1462
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-17 10:13:06 -0700

    remove user route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-router
-  .route("/")
+router.route("/")
   .post(userController.newUser);
 
-router.route("/:email").get(userController.signIn);
+router.route("/:email")
+  .get(userController.signIn);
+
+router.route("/:id")
+  .delete(userController.removeAcct);
 
 module.exports = router;
